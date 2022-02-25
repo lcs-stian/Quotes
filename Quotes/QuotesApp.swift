14461d18a4835f6e2bf902a2e2cfe9ff33f4ca0c
Suzanne <STian@lcs.on.ca>
2022-02-24 21:18:08 -0500

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Suzanne Tian on 2022-02-24.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
